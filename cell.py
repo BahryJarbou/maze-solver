c0bbc2cfbac6ff0b37b268cd2af4bd3f16fc8682
Bahry <bahry.jarbou@gmail.com>
2025-05-02 23:45:47 +0200

    final version

---
+++ cell.py
@@ -0,0 +1,51 @@
+from graphics import Line, Point
+class Cell:
+    def __init__(self, win = None):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None 
+        self._win = win
+        self.visited = False
+        
+    def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        left_wall = Line(Point(x1,y1),Point(x1,y2))
+        if self.has_left_wall:
+            self._win.draw_line(left_wall)
+        else:
+            self._win.draw_line(left_wall,"white") 
+        right_wall = Line(Point(x2,y1),Point(x2,y2))
+        if self.has_right_wall:
+            self._win.draw_line(right_wall)
+        else:
+            self._win.draw_line(right_wall,"white")
+        top_wall = Line(Point(x1,y1),Point(x2,y1))
+        if self.has_top_wall:
+            self._win.draw_line(top_wall)
+        else:
+            self._win.draw_line(top_wall, "white")
+        bottom_wall = Line(Point(x1,y2),Point(x2,y2))
+        if self.has_bottom_wall:
+            self._win.draw_line(bottom_wall)
+        else:
+            self._win.draw_line(bottom_wall, "white")
+            
+    def draw_move(self, to_cell, undo=False):
+            center1 = Point(self._x1 + abs(self._x2 - self._x1)//2, self._y1 + abs(self._y2 - self._y1)//2)
+            center2 = Point(to_cell._x1 + abs(to_cell._x2 - to_cell._x1)//2, to_cell._y1 + abs(to_cell._y2 - to_cell._y1)//2)
+            line = Line(center1, center2)
+            if undo:
+                self._win.draw_line(line, "gray")
+            else:
+                self._win.draw_line(line, "red")
+            
